ab192767c36784da1a80058ba561eed6bf2bf7c9
Grant Wilkinson <grant@aimermedia.com>
2020-08-05 13:13:33 +0100

    Updates

---
+++ precache-manifest.9e90c861258fd37669b015cb7083f7b5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96583eefc9a1f04247443396b2f39c6",
+    "url": "/alexandra-rose-gifts/index.html"
+  },
+  {
+    "revision": "a575d46ad6598b302e7d",
+    "url": "/alexandra-rose-gifts/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "822bbbe1ed39f52fd6ce",
+    "url": "/alexandra-rose-gifts/static/css/main.cf24d563.chunk.css"
+  },
+  {
+    "revision": "a575d46ad6598b302e7d",
+    "url": "/alexandra-rose-gifts/static/js/2.55548c33.chunk.js"
+  },
+  {
+    "revision": "92619aa821614f8fd12bcba1380b1773",
+    "url": "/alexandra-rose-gifts/static/js/2.55548c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822bbbe1ed39f52fd6ce",
+    "url": "/alexandra-rose-gifts/static/js/main.f90f808f.chunk.js"
+  },
+  {
+    "revision": "a0bd099201b72ad7ff0b",
+    "url": "/alexandra-rose-gifts/static/js/runtime-main.0a92d717.js"
+  },
+  {
+    "revision": "14025176bc5792e3a5a035f74182501d",
+    "url": "/alexandra-rose-gifts/static/media/ARG-logo-text.14025176.png"
+  },
+  {
+    "revision": "a568614e0784179d37066f5dddfc1084",
+    "url": "/alexandra-rose-gifts/static/media/ARG-logo.a568614e.png"
+  },
+  {
+    "revision": "f8f3e38e1879f40652c7ff41333d2ac6",
+    "url": "/alexandra-rose-gifts/static/media/Autumn-in-November.f8f3e38e.ttf"
+  },
+  {
+    "revision": "da19b977d02068a76ded1050743e75bb",
+    "url": "/alexandra-rose-gifts/static/media/Bold-&-Stylish-Calligraphy.da19b977.ttf"
+  },
+  {
+    "revision": "62d26ef0a4cba74d30776800d4a0d9b0",
+    "url": "/alexandra-rose-gifts/static/media/Flower-Shop-TTF.62d26ef0.ttf"
+  },
+  {
+    "revision": "34dcc74ae3ef577b4293b63c8c93e44c",
+    "url": "/alexandra-rose-gifts/static/media/JandaSwirlyTwirly.34dcc74a.ttf"
+  },
+  {
+    "revision": "35dadb5daea3cd4809cee3c9f7a64d53",
+    "url": "/alexandra-rose-gifts/static/media/Lumberjack-Regular.35dadb5d.ttf"
+  },
+  {
+    "revision": "c91421c7330e06f96165e3858e2fb54d",
+    "url": "/alexandra-rose-gifts/static/media/October-Twilight.c91421c7.ttf"
+  },
+  {
+    "revision": "adf215fc5f82377efe6e0fb511c69fbb",
+    "url": "/alexandra-rose-gifts/static/media/OhMyItsJuly-TTF.adf215fc.ttf"
+  },
+  {
+    "revision": "aef63819444d1ea3e9b38b83d00682a3",
+    "url": "/alexandra-rose-gifts/static/media/Unicorn-Calligraphy-TTF.aef63819.ttf"
+  }
+]);
